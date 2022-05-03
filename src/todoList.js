844020a248044c4619cfe9110791dbd0d6e4f54b
[nboyaroca] <[nboyaroca@gmail.com]>
2022-05-03 10:46:33 +0200

    todoApp

--- src/todoList.js
+++ src/todoList.js
@@ -1,12 +1,21 @@
 export const todoList = {
     state: [
-        'Estudiar TDD ',
-        'practicat katas ',
-        'Estudiar bases de dades relacionals',
+        {
+            id: 1,
+            task: 'Estudiar TDD ',
+        },
+        {
+            id: 2,
+            task: 'practicat katas ',
+        },
+        {
+            id: 3,
+            task: 'Estudiar bases de dades relacionals',
+        }
     ],
-    
+       
     addTodo(newTodo) {
-        this.state.push(newTodo);
+        this.state.push({id: 4, task: newTodo});
         this.render();
     },
 
@@ -19,16 +28,14 @@ export const todoList = {
         let html = "";
     
         for (const todo of this.state) { 
-            html += `<li>${todo} - <button id=${this.state.indexOf(todo)} class="deleteButton">delete</button></li>`;  
+            html += `<li>${todo.task} - <button id=${this.state.indexOf(todo)} class="deleteButton">delete</button></li>`;  
         }
         let DOMlist = document.getElementById("App") 
         DOMlist.innerHTML = html;
         
         let buttons = document.querySelectorAll(".deleteButton")
-        //console.log(buttons);
         buttons.forEach(button => {
-            console.log(button);
-            button.addEventListener('click', (e) => this.deleteByIndex(e.target.id));
+        button.addEventListener('click', (e) => this.deleteByIndex(e.target.id));
         });
     }
 
